d664ec538bcec5bd9ef150e75ecef60b40e42dd5
rheinatamara <rheina.tamara@outlook.com>
2022-02-16 14:55:36 +0700

    update add news

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -41,7 +41,36 @@ class NewsController {
     }
   }
   static async postNews(req, res) {
-    console.log("masuk ke controller");
+    try {
+      const { title, mainImg, categoryId, description, imgUrl } = req.body;
+      const result = await News.create({
+        title,
+        mainImg,
+        categoryId,
+        description,
+        authorId: req.user.id,
+      });
+      if (result) {
+        try {
+          await Promise.all(
+            imgUrl.map(async (el) => {
+              await Image.create({
+                imgUrl: el,
+                newsId: result.id,
+              });
+            })
+          );
+
+          res.status(201).json(result);
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        res.status(400).json({ message: "Cannot create a news" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
   }
 }
 module.exports = NewsController;
--- server/routes/newsRouter.js
+++ server/routes/newsRouter.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 const NewsController = require("../controllers/newsController");
 const uploadFiles = require("../middlewares/multer");
 const authorization = require("../middlewares/authorization");
+const imageKit = require("../middlewares/imagekit");
 router.get("/", NewsController.allNews);
 router.get("/news/:id", NewsController.getById);
 router.get("/categories", NewsController.getCategories);
-router.post("/news", authorization, uploadFiles, NewsController.postNews);
+router.post(
+  "/news",
+  authorization,
+  uploadFiles,
+  imageKit,
+  NewsController.postNews
+);
 module.exports = router;
