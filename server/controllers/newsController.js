624f51f12b1501cbf83003f514b3bde8b668a329
rheinatamara <rheina.tamara@outlook.com>
2022-02-16 13:02:21 +0700

    update server on imagekit, auth, multer

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -12,5 +12,36 @@ class NewsController {
       res.status(500).json({ message: "Internal server error" });
     }
   }
+  static async getById(req, res) {
+    const id = +req.params.id;
+    try {
+      const data = await News.findByPk(id, {
+        include: [Category, User, Image],
+      });
+
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({ message: "News not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+  static async getCategories(req, res) {
+    try {
+      const categories = await Category.findAll();
+      if (categories) {
+        res.status(200).json(categories);
+      } else {
+        res.status(404).json({ message: "Categories not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+  static async postNews(req, res) {
+    console.log("masuk ke controller");
+  }
 }
 module.exports = NewsController;
--- server/routes/newsRouter.js
+++ server/routes/newsRouter.js
@@ -1,4 +1,9 @@
 const router = require("express").Router();
 const NewsController = require("../controllers/newsController");
+const uploadFiles = require("../middlewares/multer");
+const authorization = require("../middlewares/authorization");
 router.get("/", NewsController.allNews);
+router.get("/news/:id", NewsController.getById);
+router.get("/categories", NewsController.getCategories);
+router.post("/news", authorization, uploadFiles, NewsController.postNews);
 module.exports = router;
